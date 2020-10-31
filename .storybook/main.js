30442857f6c081c4f56e536de607ee1c7f4037d2
Nedim Becirovic <nedimb86@gmail.com>
2020-10-31 10:26:31 +0100

    Fix glob

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/preset-typescript',
     '@storybook/preset-create-react-app',
